8e7dc94ab8ee346d62ef2e50e2744f36799cb971
shir <shir8806@gmail.com>
2023-01-22 21:59:47 +0200

    installations

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { request, response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const port = 3333;
+
+app.use(cors({origin: "http://localhost:3333"}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+
+app.listen(port, '0.0.0.0', ()=> {
+    console.log(`App is running on localhost:${port}`)
+  })
