bcf40ba6c2562ab3310789a0ca9baed166fadbb8
shir <shir8806@gmail.com>
2023-01-24 19:41:13 +0200

    fetch word from the server

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,23 @@ import cors from 'cors';
 export const app = express();
 const port = 3333;
 
-app.use(cors({origin: "http://localhost:3333"}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.get('/game/choose-word', (req, res) => {
+    res.send('lemon');
+  });
+  
+app.post('/guess', (req, res) => {
+// Code to handle a guess submission
+});
+
+app.get('/game-status/:id', (req, res) => {
+// Code to check the status of a game
+});
 
 
-// app.listen(port, '0.0.0.0', ()=> {
-//     console.log(`App is running on localhost:${port}`)
-//   })
+export const wordBank = [];
+
+
